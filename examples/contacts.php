e9919176f40158f34e34a6496b03d726d400c924
Phillip J. Roth <PhillipRoth@gmail.com>
2017-03-02 11:24:46 -0500

    Update contacts.php

--- examples/contacts.php
+++ examples/contacts.php
@@ -1,12 +1,21 @@
 <?php
 
 /*
- *  To create a HubSpot contact, contact data must be passed in via properties
+ *  To create a HubSpot contact, contact data must be passed in via properties.
+ *  After the request, the HubSpot contact object is returned.
+ *
  *  http://developers.hubspot.com/docs/methods/contacts/create_contact
  */
-$this->hubspot->contacts()->create([
+$createContact = $this->hubspot->contacts()->create([
   ['property' => 'firstname', 'value' => 'Phillip'],
   ['property' => 'lastname', 'value' => 'Roth'],
   ['property' => 'email', 'value' => 'philliproth@gmail.com'],
   ['property' => 'company', 'value' => 'Giftogram'],
 ]);
+
+/*
+ * To retrieve a single HubSpot contact, call contacts()->getByEmail
+ */
+$contact = $this->hubspot->contacts()->getByEmail(
+  $createContact->data->properties->email->value
+);
