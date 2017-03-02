2541c0939e019b849bc1deb52b4e5a32ba6124e7
Phillip J. Roth <PhillipRoth@gmail.com>
2017-03-02 11:18:22 -0500

    Adds documentation for creating contacts

---
+++ examples/contacts.php
@@ -0,0 +1,10 @@
+/*
+ *  To create a HubSpot contact, contact data must be passed in via properties
+ *  http://developers.hubspot.com/docs/methods/contacts/create_contact
+ */
+$this->hubspot->contacts()->create([
+  ['property' => 'firstname', 'value' => 'Phillip'],
+  ['property' => 'lastname', 'value' => 'Roth'],
+  ['property' => 'email', 'value' => 'philliproth@gmail.com'],
+  ['property' => 'company', 'value' => 'Giftogram'],
+]);
